46154e6646151adec7fbddc87e3cd5352f1a793f
Guy Edri <127855018+GuyEdri8@users.noreply.github.com>
2025-01-18 17:10:53 +0200

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# helloworld.py
+
+def print_message():
+    print("Hello World! from DevOps Lab")
---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+
+# Importing the function from helloworld.py
+from helloworld import print_message
+
+# Trigger the function
+if __name__ == "__main__":
+    print_message()
